15d196adfe2581db2c7e255792dee106a55d9e56
Noor Alam <alam17058@gmail.com>
2023-10-12 23:02:17 +0530

    first commit

---
+++ src/components/Box.js
@@ -0,0 +1,16 @@
+"use client"
+import React from "react";
+import "./Box.css";
+
+const Box = ({ value, onClick }) => {
+  //assigning clss name depending on the value
+  const style = value === "X" ? "box x" : "box o";
+
+  return (
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Box;
